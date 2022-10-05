63d74b8b2f018334dedddfe346223f94c50847ae
StanDimitrov <miro_8888@abv.bg>
2022-10-05 12:11:28 +0200

    second method to configurate your selenium webdriver

---
+++ web-end-to-end.py
@@ -0,0 +1,9 @@
+from this import d
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("https://www.google.com")
+print("Title of this link is: ", driver.title)
+print("Title of this url is: ", driver.url)
